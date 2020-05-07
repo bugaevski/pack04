47f6f56f9adbc22daef0a1709b1b2cf18552783c
Michael Bugaevski <mikejm618@gmail.com>
2020-05-07 15:09:35 -0400

    v1.0.1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pack04
+
+go 1.14
---
+++ pack04.go
@@ -0,0 +1,10 @@
+package pack04
+
+//S:\MiGo\mibGOPATH\src\mibpacks\usePack04\pack04\pack04.go
+//
+
+//HiFromMipack04 return heloo
+func HiFromMipack04() string {
+	var returnedValue = "Hello from Mikki at HiFromMipack04"
+	return returnedValue
+}
